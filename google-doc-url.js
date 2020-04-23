693a8bbf9aebd42726b00bccdc2b49db56ab27b4
collync <35468121+collync@users.noreply.github.com>
2020-04-23 01:31:36 -0400

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ZxvU8eGyuN9M8GxTU9acKVJv70iC3px_m3EVFsOHN9g/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1NvZeUXw2a-7qZB84ePbFC2jYCiaFNpZaOxCnotp8MlA/edit#gid=0';
